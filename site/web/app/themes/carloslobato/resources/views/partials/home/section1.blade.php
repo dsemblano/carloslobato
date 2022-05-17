68b7e73793a3f435ed6aa11f8985e9f35a26ab75
Daniel Semblano <dsemblano@gmail.com>
2022-05-17 11:37:33 -0300

    change Prós string

--- site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
@@ -1,21 +1,22 @@
 <div class="px-3 mx-auto px-4 flex flex-wrap flex-col md:flex-row items-center bg-white border-bs container">
   <!--Left Col-->
   <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-    <p class="uppercase tracking-loose w-full">Prós 90 Amapá</p>
+    <p class="uppercase tracking-loose w-full">Pros 90 Amapá</p>
     <h1 class="my-4 text-5xl font-bold leading-tight">Carlos Lobato</h1>
     <p class="leading-normal text-2xl mb-8">
-      Advogado, radialista e jornalista, nascido em 16/11/1956 é pré candidato a Deputado Estadual pelo Prós 90 Amapá.
+      Advogado, radialista e jornalista, nascido em 16/11/1956 é pré candidato a Deputado Estadual pelo Pros 90 Amapá.
     </p>
     <p>
-    #naocala
+      #naocala
     </p>
     <div class="social hidden lg:block">
       <p class="mb-8">
-      @include('partials/snippets.social')
+        @include('partials/snippets.social')
       </p>
       <p class="mt-8">
-      @include('partials/snippets.facebook')
+        @include('partials/snippets.facebook')
       </p>
+
     </div>
 
   </div>
@@ -24,11 +25,11 @@
     <img class="w-full md:w-4/5 z-50 rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
   </div>
   <div class="social block lg:hidden text-center">
-      <p class="mb-8">
+    <p class="mb-8">
       @include('partials/snippets.social')
-      </p>
-      <p class="mt-8">
+    </p>
+    <p class="mt-8">
       @include('partials/snippets.facebook')
-      </p>
-    </div>
+    </p>
+  </div>
 </div>
