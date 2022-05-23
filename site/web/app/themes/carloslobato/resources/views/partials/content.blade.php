36e412936a28f4405cc2e830a9e216cb74ffeeda
Daniel Semblano <dsemblano@gmail.com>
2022-05-23 18:17:31 -0300

    noticas page v1.1

--- site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
@@ -1,10 +1,12 @@
 <article @php(post_class('article-posts'))>
   <figure class="thumbnail">
+    <a href="{{ get_permalink() }}">
     @if ( has_post_thumbnail() )
       {{ the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full w-full object-cover object-center rounded-lg shadow-md', 'title' => 'Feature image']); }}
       @else
       <img class="img-responsive responsive--full w-full object-cover object-center rounded-lg shadow-md" src="@asset('images/default.png')"  alt="{!! $title !!}">
     @endif
+    </a>
   </figure>
     <header>
       <h2 class="entry-title mt-1 text-xl font-semibold uppercase leading-tight truncate">
