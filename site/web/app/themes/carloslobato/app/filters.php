f0026df63cbaa0a0b9dc5a58714ee004f266a7bd
Daniel Semblano <dsemblano@gmail.com>
2022-05-24 18:32:07 -0300

    remove url input from commment

--- site/web/app/themes/carloslobato/app/filters.php
+++ site/web/app/themes/carloslobato/app/filters.php
@@ -14,3 +14,11 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <p class="mt-2"><a href="%s">%s</a></p>', get_permalink(), __('Leia Mais', 'sage'));
 });
+
+
+// remove url from commment
+add_filter( 'comment_form_default_fields', function($fields) {
+    if(isset($fields['url']))
+    unset($fields['url']);
+    return $fields;
+});
