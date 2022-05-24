2f1e4579de43fa136aa2df52dea791cc9dd3e18b
Daniel Semblano <dsemblano@gmail.com>
2022-05-24 12:33:01 -0300

    page noticia posts formatting v2.1

--- site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
@@ -10,7 +10,7 @@
       </a>
     </figure>
       <header>
-        <h2 class="tracking-widest text-indigo-500 text-xs font-medium title-font">
+        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
           <a href="{{ get_permalink() }}">
             {!! $title !!}
           </a>
@@ -24,13 +24,3 @@
       </div>
   </div>
 </article>
-
-<div class="xl:w-1/3 md:w-1/2 p-4">
-  <div class="bg-white p-6 rounded-lg">
-    <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="https://kuyou.id/content/images/ctc_2020021605150668915.jpg" alt="Image Size 720x400">
-    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-  </div>
-</div>
- 
