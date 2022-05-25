2e30e0d40b7aced4156fc610c78dd552fe0bafe9
Daniel Semblano <dsemblano@gmail.com>
2022-05-25 16:33:10 -0300

    page header text-center

--- site/web/app/themes/carloslobato/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="page-header container mb-10 prose lg:prose-xl prose-img:rounded-xl prose-a:text-blue-600 hover:prose-a:text-blue-500">
+<div class="page-header text-center container mb-10 prose lg:prose-xl prose-img:rounded-xl prose-a:text-blue-600 hover:prose-a:text-blue-500">
   <h1>{!! $title !!}</h1>
 </div>
