6555d436066beb93f3c90547caf99669b17faca3
Daniel Semblano <dsemblano@gmail.com>
2022-05-26 19:44:28 -0300

    footer v1.1

--- site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
+++ site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
@@ -1,8 +1,9 @@
-<footer class="content-info bg-pros90orange mt-8 py-10">
+<footer class="content-info bg-pros90orange mt-8 py-10 px-10">
   <p class="text-center text-white">
-    <a href="https://api.whatsapp.com/send?phone=5596991217272" title="Mande sua mensagem via Whatsapp para NØS!"
-      target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 9121-7272</a> Carlos
-    Lobato © Todos os direitos reservados. {{ date('Y') }}
+    <a href="https://api.whatsapp.com/send?phone=5596991217272" title="Mande sua mensagem via Whatsapp para nossa equipe!"
+    target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 9121-7272
+    </a> 
+    <span>Carlos Lobato © Todos os direitos reservados. {{ date('Y') }}</span>
   </p>
   @php(dynamic_sidebar('sidebar-footer'))
   {{-- @include('partials/home.section2') --}}
