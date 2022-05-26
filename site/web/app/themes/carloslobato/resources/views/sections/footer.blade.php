35fb63a952117413355828c5bbfe251b2c29cb19
Daniel Semblano <dsemblano@gmail.com>
2022-05-26 18:30:16 -0300

    css media print fix

--- site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
+++ site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
@@ -1,4 +1,9 @@
-<footer class="content-info">
+<footer class="content-info bg-gray-500">
+  <p class="text-center">
+    <a href="https://api.whatsapp.com/send?phone=5596991217272" title="Mande sua mensagem via Whatsapp para NØS!"
+      target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 9121-7272</a> Carlos
+    Lobato © Todos os direitos reservados. {{ date('Y') }}
+  </p>
   @php(dynamic_sidebar('sidebar-footer'))
-  @include('partials/home.section2')
+  {{-- @include('partials/home.section2') --}}
 </footer>
