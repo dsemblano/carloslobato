27e473492bb425bd0aae1608bdd3c5b47020c699
Daniel Semblano <dsemblano@gmail.com>
2022-05-30 18:59:48 -0300

    Wordpress Search form improv thumbnail v1.3

--- site/web/app/themes/carloslobato/resources/views/partials/content-search.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/content-search.blade.php
@@ -1,4 +1,4 @@
-<article @php(post_class())>
+<article @php(post_class('buscateste'))>
   <header>
     <h2 class="entry-title">
       <a href="{{ get_permalink() }}">
@@ -10,6 +10,15 @@
   </header>
 
   <div class="entry-summary">
+    <figure class="thumbnail">
+      <a href="{{ get_permalink() }}">
+      @if ( has_post_thumbnail() )
+        {{ the_post_thumbnail('post-thumbnail', ['class' => 'lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6', 'title' => 'Feature image']); }}
+        @else
+        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="@asset('images/default.png')"  alt="{!! $title !!}">
+      @endif
+      </a>
+    </figure>
     @php(the_excerpt())
   </div>
 </article>
