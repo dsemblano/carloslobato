ab794632fda9c1c7f8f59d115bacc8939e69541b
Daniel Semblano <dsemblano@gmail.com>
2022-05-30 12:22:32 -0300

    whatsapp fix number

--- site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
+++ site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="content-info bg-pros90orange mt-8 py-10 px-10">
   <p class="text-center text-white">
-    <a href="https://api.whatsapp.com/send?phone=5596991217272" title="Mande sua mensagem via Whatsapp para nossa equipe!"
-    target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 9121-7272
+    <a href="https://api.whatsapp.com/send?phone=5596981086567" title="Mande sua mensagem via Whatsapp para nossa equipe!"
+    target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 98108-6567
     </a>
     <br> 
     <span>Carlos Lobato © Todos os direitos reservados. {{ date('Y') }}</span>
