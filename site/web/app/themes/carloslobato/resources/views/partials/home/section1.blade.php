45c66c348ba4d3e66f6f444af77042c90ea9f70d
Daniel Semblano <dsemblano@gmail.com>
2022-06-02 12:17:49 -0300

    refactoring socialicons

--- site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
@@ -6,7 +6,10 @@
       Amapaense, advogado, psicólogo, sociólogo, jornalista e âncora do programa Tribuna da Cidade na 1.
     </p>
     <div class="social hidden lg:block w-full">
-        @include('partials/snippets.social')
+      <div class="mb-6 flex flex-col fill-pros90blue">
+        @include('partials/snippets.socialicons')
+        @include('partials/snippets.fblikeshare')
+      </div>
         @include('partials/snippets.facebook')
       {{-- <h2>Entre em contato</h2>
       @php(dynamic_sidebar('textos-primary')) --}}
@@ -17,8 +20,8 @@
   <div class="w-full md:w-3/5 py-6 text-center">
     <img class="w-full md:w-4/5 z-50 rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
   </div>
-  <div class="social block lg:hidden text-center w-full">
-      @include('partials/snippets.social')
+  <div class="social block lg:hidden text-center w-full fill-pros90blue">
+      @include('partials/snippets.socialicons')
       @include('partials/snippets.facebook')
   </div>
 </section>
--- site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
+++ site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
@@ -1,11 +1,14 @@
 <footer class="content-info bg-pros90orange mt-7 py-8 px-10">
-  <p class="text-center text-white">
+  <div class="text-center text-white">
     <a href="https://api.whatsapp.com/send?phone=5596981086567" title="Mande sua mensagem via Whatsapp para nossa equipe!"
     target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (96) 98108-6567
     </a>
     <br> 
     <span>Carlos Lobato © Todos os direitos reservados. {{ date('Y') }}</span>
-  </p>
+  </div>
+  <div class="text-center text-white fill-white mt-4">
+    @include('partials/snippets.socialicons')
+  </div>
   @php(dynamic_sidebar('sidebar-footer'))
   {{-- @include('partials/home.section2') --}}
 </footer>
