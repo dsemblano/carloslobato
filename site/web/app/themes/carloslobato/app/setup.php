f788e44e4b7c807a5affb6b93237a438b7a4de85
Daniel Semblano <dsemblano@gmail.com>
2022-06-13 19:15:20 -0300

    goog tag body and head code

--- site/web/app/themes/carloslobato/app/setup.php
+++ site/web/app/themes/carloslobato/app/setup.php
@@ -142,6 +142,10 @@ add_action('wp_head', function () {
     include get_template_directory() . '/resources/views/partials/snippets/gtag.js';
     include get_template_directory() . '/resources/views/partials/snippets/gconsoleverif.php';
     include get_template_directory() . '/resources/views/partials/snippets/clarity.php';
+    include get_template_directory() . '/resources/views/partials/snippets/gtagbody.js';
+    include get_template_directory() . '/resources/views/partials/snippets/gtaghead.js';
+}, 100);
+}, 100);
 }, 100);
 
 // Remove svg duotone
