7444bac9932e75da158451d1211e0bf72e655cf2
Daniel Semblano <dsemblano@gmail.com>
2022-06-24 14:26:03 -0300

    block plugins and Noticias css improvements

--- site/web/app/themes/carloslobato/resources/views/partials/content-search.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/content-search.blade.php
@@ -1,5 +1,5 @@
-<article @php(post_class('article-posts xl:w-1/3 md:w-1/2 mt-4'))>
-  <div class="bg-white p-6 rounded-lg">
+<article @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-8 xl:pb-8'))>
+  <div class="bg-white p-6">
     <figure class="thumbnail">
       <a href="{{ get_permalink() }}">
       @if ( has_post_thumbnail() )
--- site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/content.blade.php
@@ -1,24 +1,25 @@
-<article @php(post_class('article-posts xl:w-1/3 md:w-1/2 mt-4'))>
-  <div class="bg-white p-6 rounded-lg">
+<article @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-8 xl:pb-8'))>
+  <div class=" box bg-white ">
     <figure class="thumbnail">
       <a href="{{ get_permalink() }}">
-      @if ( has_post_thumbnail() )
-        {{ the_post_thumbnail('post-thumbnail', ['class' => 'lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6', 'title' => 'Feature image']); }}
+        @if ( has_post_thumbnail() )
+        {{ the_post_thumbnail('post-thumbnail', ['class' => 'lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 w-full object-cover object-center', 'title' => 'Feature image']); }}
         @else
-        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="@asset('images/default.png')"  alt="{!! $title !!}">
-      @endif
+        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 w-full object-cover object-center"
+          src="@asset('images/default.png')" alt="{!! $title !!}">
+        @endif
       </a>
     </figure>
-      <header>
-        <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-          <a href="{{ get_permalink() }}">
-            {!! $title !!}
-          </a>
-        </h2>
-    
+    <header class="p-6">
+      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+        <a href="{{ get_permalink() }}">
+          {!! $title !!}
+        </a>
+      </h2>
+
       @include('partials.entry-meta')
-      </header>
-        <?php
+    </header>
+    <?php
           //echo '<p class="leading-relaxed text-base text-gray-600 mt-2">' . get_the_excerpt() . '</p>'; 
         ?>
   </div>
