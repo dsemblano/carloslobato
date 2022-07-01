9e5e24f2aa4b1ded35fb7274a867f50a623f74dd
Daniel Semblano <dsemblano@gmail.com>
2022-07-01 12:55:47 -0300

    Home css improvements

--- site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
@@ -1,10 +1,17 @@
-<section class="section1 flex flex-wrap flex-col md:flex-row items-center bg-white border-bs container">
+<section id="section1" class="flex flex-col lg:flex-row justify-between items-start bg-white">
   <!--Left Col-->
-  <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-    <h1 class="my-4 text-5xl font-bold leading-tight">Carlos Lobato</h1>
-    <p class="leading-normal text-xl mb-8">
-      Amapaense, advogado, psicólogo, sociólogo, jornalista e âncora do Tribuna da Cidade na 101 FM
-    </p>
+  <div class="flex flex-col w-full lg:w-2/5 justify-center text-center lg:text-left">
+    <h1 class="my-4 uppercase text-7xl lg:text-9xl font-bold">Carlos Lobato</h1>
+    <article>
+      <p class="leading-normal text-xl mb-8">
+        Amapaense, advogado, psicólogo, sociólogo, jornalista e âncora do Tribuna da Cidade na 101 FM
+      </p>
+      <p class="leading-normal text-xl mb-8">
+        Nascido em Macapá, no dia 16 de novembro de 1956, Carlos Lobato aprendeu desde cedo o valor de boas escolhas. Dedicado
+        aos estudos, o menino pobre, que vendeu sacolas no Mercado Central, cresceu e passou em 22 vestibulares. Venceu na
+        vida, tornou-se psicólogo, sociólogo, advogado e jornalista.
+      </p>
+    </article>
     <div class="social hidden lg:block w-full">
       <div class="mb-6 flex flex-col fill-pros90blue">
         @include('partials/snippets.socialicons')
@@ -17,11 +24,11 @@
 
   </div>
   <!--Right Col-->
-  <div class="w-full md:w-3/5 py-6 text-center">
-    <img class="w-full md:w-4/5 z-50 rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
-  </div>
-  <div class="social block lg:hidden text-center w-full fill-pros90blue">
+  <div class="pl-0 lg:pl-40">
+    <img class="z-50 rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
+    <div class="social block lg:hidden text-center w-full fill-pros90blue">
       @include('partials/snippets.socialicons')
       @include('partials/snippets.facebook')
+  </div>
   </div>
 </section>
