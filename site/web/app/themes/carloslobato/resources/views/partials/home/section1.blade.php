393636e7c8eb369ae808729683e75b29360e1843
Daniel Semblano <dsemblano@gmail.com>
2022-07-01 15:47:50 -0300

    Home css mobile improvements

--- site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
@@ -2,6 +2,7 @@
   <!--Left Col-->
   <div class="flex flex-col w-full lg:w-2/5 justify-center text-center lg:text-left">
     <h1 class="my-4 uppercase text-7xl lg:text-9xl font-bold">Carlos Lobato</h1>
+    <img class="block lg:hidden rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
     <article>
       <p class="leading-normal text-xl mb-8">
         Amapaense, advogado, psicólogo, sociólogo, jornalista e âncora do Tribuna da Cidade na 101 FM
@@ -12,23 +13,20 @@
         vida, tornou-se psicólogo, sociólogo, advogado e jornalista.
       </p>
     </article>
-    <div class="social hidden lg:block w-full">
-      <div class="mb-6 flex flex-col fill-pros90blue">
-        @include('partials/snippets.socialicons')
-        @include('partials/snippets.fblikeshare')
-      </div>
-        @include('partials/snippets.facebook')
-      {{-- <h2>Entre em contato</h2>
-      @php(dynamic_sidebar('textos-primary')) --}}
-    </div>
-
   </div>
-  <!--Right Col-->
-  <div class="pl-0 lg:pl-40">
-    <img class="z-50 rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
-    <div class="social block lg:hidden text-center w-full fill-pros90blue">
-      @include('partials/snippets.socialicons')
-      @include('partials/snippets.facebook')
+    <!--Right Col desktop -->
+  <div class="social hidden lg:block w-full bg-yellow-400">
+    <img class="z-50 hidden lg:block rounded-tl-extraLarge rounded-br-extraLarge" src="@asset('images/perfil.jpg')" />
+    {{-- <h2>Entre em contato</h2>
+    @php(dynamic_sidebar('textos-primary')) --}}
   </div>
-  </div>
+  <!--Right Col mobile -->
 </section>
+
+<section class="socialmidia">
+  @include('partials/snippets.socialicons')
+  @include('partials/snippets.fblikeshare')
+</section>
+<div>
+  @include('partials/snippets.facebook')
+</div>
