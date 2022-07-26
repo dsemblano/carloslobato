fe8984104d1099c95982edc624bd432f337ae761
Daniel Semblano <dsemblano@gmail.com>
2022-07-26 16:56:58 -0300

    Profile photo changed

--- site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/section1.blade.php
@@ -2,7 +2,7 @@
   <!--Left Col-->
   <div class="flex flex-col w-full lg:w-2/5 justify-center text-center lg:text-left animated animatedFadeInUp fadeInUp">
     <h1 class="mb-4 uppercase text-7xl lg:text-9xl font-bold text-pros90lightblue">Carlos Lobato</h1>
-    <img class="block lg:hidden rounded-tl-extraLarge rounded-br-extraLarge" width="834" height="834" alt="Imagem Carlos Lobato principal na home" src="@asset('images/perfil.jpg')" />
+    <img class="block lg:hidden rounded-tl-extraLarge rounded-br-extraLarge" width="834" height="834" alt="Imagem Carlos Lobato principal na home" src="@asset('images/Carlos Lobato foto perfil.png')" />
     <article class="mt-8 mb-8 prose lg:prose-2xl  prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline">
       <p class="leading-normal text-xl mb-8">
         Candidato a Deputado Estadual pelo <a class="underline" target="_blank" href="https://pros90amapa.org.br">PROS Amapá</a>, advogado, psicólogo, sociólogo, jornalista e ex-âncora do Tribuna da Cidade na 101 FM.
@@ -21,7 +21,7 @@
   </div>
     <!--Right Col desktop -->
   <div class="social hidden lg:block w-full pl-48">
-    <img class="z-50 hidden lg:block rounded-tl-extraLarge rounded-br-extraLarge" width="834" height="834" alt="Imagem Carlos Lobato principal na home" src="@asset('images/perfil.jpg')" />
+    <img class="z-50 hidden lg:block rounded-tl-extraLarge rounded-br-extraLarge" width="834" height="834" alt="Imagem Carlos Lobato principal na home" src="@asset('images/Carlos Lobato foto perfil.png')" />
     {{-- <h2>Entre em contato</h2>
     @php(dynamic_sidebar('textos-primary')) --}}
   </div>
