8ea9b4f48100b65148aac4596502faca886d4f6e
Daniel Semblano <dsemblano@gmail.com>
2022-08-19 15:02:25 -0300

    video high quality webm home with controls

--- site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
@@ -1,3 +1,3 @@
 <section class="relative herovideo">
-    <video playsinline="" loop="" muted="" autoplay="" data-src="@asset('images/lobatovideo.webm')" src="@asset('images/lobatovideo.webm')"></video>
+    <video controls playsinline="" loop="" muted="" autoplay="" data-src="@asset('images/lobatovideo.webm')" src="@asset('images/lobatovideo.webm')"></video>
 </section>
