8db090035fc2f11a5831d7c5a1f74c4e068603ba
Daniel Semblano <dsemblano@gmail.com>
2022-08-21 22:20:43 -0300

    header fixes for mobile, desktop and responsiveness for ipad

--- site/web/app/themes/carloslobato/resources/views/template-home.blade.php
+++ site/web/app/themes/carloslobato/resources/views/template-home.blade.php
@@ -8,6 +8,6 @@
     @include('partials/home.section1')
     {{-- {!! do_shortcode('[wdi_feed id="1"]') !!} --}}
     @include('partials/home.lobatovideo')
-    {!! do_shortcode('[instagram-feed feed=1 class="container px-8"]') !!}
+    {!! do_shortcode('[instagram-feed feed=1 class="container px-8 lg:px-16"]') !!}
     {{-- {!! do_shortcode('[contact-form-7 id="20" title="Formulario"]') !!} --}}
 @endsection
