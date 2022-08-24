925ce0e83fd024a32f4fc5ba5c8e737055fd3bed
Daniel Semblano <dsemblano@gmail.com>
2022-08-24 12:10:59 -0300

    Days for election font size

--- site/web/app/themes/carloslobato/resources/views/partials/snippets/faltamdias.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/snippets/faltamdias.blade.php
@@ -1,5 +1,5 @@
 <div id="faltamdias" class="container px-8 lg:px-16 flex flex-col items-center">
     <h2 class="text-pros90blue uppercase text-6xl">Faltam</h2>
     {!! do_shortcode('[countdown date="02 October 2022" hour="07"  color="#F78F22" width="250" height="200" format="d" class="rounded-full radius="20"]') !!}
-    <h2 class="text-pros90blue uppercase text-6xl">paras as eleições 2022</h2>
+    <h2 class="text-pros90blue uppercase text-6xl text-center">paras as eleições 2022</h2>
 </div>
