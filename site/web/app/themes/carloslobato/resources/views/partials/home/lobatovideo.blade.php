80c3007e6b23e4d139b674654e58c623ee5d787e
Daniel Semblano <dsemblano@gmail.com>
2022-08-25 09:54:23 -0300

    lobato video mp4 home to support Safari fix

--- site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
@@ -1,4 +1,6 @@
 <section class="relative herovideo">
-    <source data-src="@asset('images/lobatovideo.mp4')" src="@asset('images/lobatovideo.mp4')" type="video/mp4" />
-    <video controls playsinline="" loop="" muted="" autoplay="" data-src="@asset('images/lobatovideo.webm')" src="@asset('images/lobatovideo.webm')"></video>
+    <video controls class="video" autoplay loop poster="@asset('images/covervideo.png')" data-setup="{}">
+        <source src="@asset('images/lobatovideo.mp4')" type="video/mp4" />
+        <source src="@asset('images/lobatovideo.webm')" type="video/webm" />
+    </video>
 </section>
