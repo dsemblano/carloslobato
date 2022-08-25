363b03344cb416cf6d820073370e1b6ca441a6c3
Daniel Semblano <dsemblano@gmail.com>
2022-08-25 16:35:53 -0300

    lobato video home muted

--- site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
@@ -1,5 +1,5 @@
 <section class="relative herovideo">
-    <video controls class="video" autoplay loop poster="@asset('images/covervideo.png')" data-setup="{}">
+    <video controls class="video" autoplay muted loop poster="@asset('images/covervideo.png')" data-setup="{}">
         <source src="@asset('images/lobatovideo.mp4')" type="video/mp4" />
         <source src="@asset('images/lobatovideo.webm')" type="video/webm" />
     </video>
