9b42ee64209ca5f4b6c33fab5f691d829309cee9
Daniel Semblano <dsemblano@gmail.com>
2022-08-25 09:16:13 -0300

    lobato video mp4 home to support Safari

--- site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/home/lobatovideo.blade.php
@@ -1,3 +1,4 @@
 <section class="relative herovideo">
+    <source data-src="@asset('images/lobatovideo.mp4')" src="@asset('images/lobatovideo.mp4')" type="video/mp4" />
     <video controls playsinline="" loop="" muted="" autoplay="" data-src="@asset('images/lobatovideo.webm')" src="@asset('images/lobatovideo.webm')"></video>
 </section>
