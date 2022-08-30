0e137653db70a99c0615db8bdfe1b4cd13b7b1fc
Daniel Semblano <dsemblano@gmail.com>
2022-08-30 11:21:32 -0300

    Figurinhas stickers homepage centered

--- site/web/app/themes/carloslobato/resources/views/partials/snippets/figurinhas.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/snippets/figurinhas.blade.php
@@ -1,7 +1,7 @@
 <div id="figurinhas" class="prose prose-a:no-underline mt-8 lg:mt-0">
     <h3 class="text-pros90blue font-bold text-3xl text-center">Baixe nossas figurinhas</h3>
     <p class="text-pros90blue font-bold text-2xl text-center pb-0 mb-0">Carlos Lobato 90000</p>
-    <div class="flex flex-row items-center">
+    <div class="flex flex-row items-center justify-center">
         <a href="https://sticker.ly/s/XK5DOQ">
             <img alt="Imagem hashtag to com lobato" src="@asset('images/perfume.jpg')" />
         </a>
