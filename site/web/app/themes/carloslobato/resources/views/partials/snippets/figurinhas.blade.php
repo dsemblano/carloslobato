c75135bd0c19ad83c9f3fefccd423d1a8f462ed2
Daniel Semblano <dsemblano@gmail.com>
2022-08-30 11:09:01 -0300

    Figurinhas stickers homepage download improv

--- site/web/app/themes/carloslobato/resources/views/partials/snippets/figurinhas.blade.php
+++ site/web/app/themes/carloslobato/resources/views/partials/snippets/figurinhas.blade.php
@@ -1,11 +1,12 @@
 <div id="figurinhas" class="prose prose-a:no-underline mt-8 lg:mt-0">
-    <h3 class="text-pros90blue font-bold text-3xl text-center">Baixe nossas figurinhas - Carlos Lobato 90000</h3>
+    <h3 class="text-pros90blue font-bold text-3xl text-center">Baixe nossas figurinhas</h3>
+    <p class="text-pros90blue font-bold text-2xl text-center pb-0 mb-0">Carlos Lobato 90000</p>
     <div class="flex flex-row items-center">
         <a href="https://sticker.ly/s/XK5DOQ">
             <img alt="Imagem hashtag to com lobato" src="@asset('images/perfume.jpg')" />
         </a>
         <a href="https://sticker.ly/s/XK5DOQ">
-            <button class="text-white bg-pros90orange hover:bg-pros90orangedark font-bold text-lg p-6 rounded-2xl">Download</button>
+            <button class="text-white bg-pros90orange hover:bg-pros90orangedark font-bold text-lg p-6 rounded-2xl animate__animated animate__shakeX">Download</button>
         </a>
     </div>
 </div>
