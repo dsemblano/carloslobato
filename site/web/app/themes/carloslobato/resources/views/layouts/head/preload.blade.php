28693f7ebb4373baa3475480da0b70e96d4e3a1b
Daniel Semblano <dsemblano@gmail.com>
2022-08-30 15:25:07 -0300

    preload rutan black fonts

--- site/web/app/themes/carloslobato/resources/views/layouts/head/preload.blade.php
+++ site/web/app/themes/carloslobato/resources/views/layouts/head/preload.blade.php
@@ -1,2 +1,5 @@
 <link rel="preload" as="image" href="@asset('images/CLperfil.png')">
 <link rel="preload" as="image" href="@asset('images/logo.png')">
+<link rel="preload" href="@asset('fonts/rutan-black-webfont.woff2')" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="@asset('fonts/rutan-black-webfont.woff')" as="font" type="font/woff" crossorigin>
+<link rel="preload" href="@asset('fonts/rutan-black-webfont.ttf')" as="font" type="font/ttf" crossorigin>
