d9c0f77c7b2c36ac352c4aa3bc7a18d2478385e9
Daniel Semblano <dsemblano@gmail.com>
2022-09-29 11:35:05 -0300

    privacy page at footer included

--- site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
+++ site/web/app/themes/carloslobato/resources/views/sections/footer.blade.php
@@ -13,7 +13,13 @@
       Eleição 20222 Carlos Alberto Lobato Lima Deputado Estadual | CNPJ: 47.316.155/0001-03 | Partido Isolado: PROS
     </div>
     <div class="text-center text-white fill-white">
+      <a href="/politica-de-privacidade/">Política de Privacidade</a>
+    </div> 
+    <div class="text-center text-white fill-white">
       Carlos Lobato © Todos os direitos reservados. {{ date('Y') }}
+    </div>
+    <div class="">
+
     </div>
   </div>
 </footer>
